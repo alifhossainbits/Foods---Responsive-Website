95f1b0093671b760a0821ccd6da4daeb45b72189
alifhossainbits <alifhossain5577@gmail.com>
2023-12-30 08:01:29 +0600

    update 8: navbar completed

--- script.js
+++ script.js
@@ -1,5 +1,18 @@
 "use strict";
 
+// navbar start
+
+document.querySelector(".menu-nav").addEventListener("click", () => {
+  console.log(54);
+  document.querySelectorAll(".target").forEach((item) => {
+    item.classList.toggle("change");
+  });
+});
+
+// navbar end
+
+// section-1 icons loop start
+
 const icons = document.querySelectorAll(".section-1-icons i");
 let i = 1;
 setInterval(() => {
@@ -19,3 +32,7 @@ setInterval(() => {
 //     icon.classList.remove("change");
 //     icon.nextElementSibling.classList.add("change");
 //   }, 1000);
+
+// section-1 icons loop end
+
+// navbar
